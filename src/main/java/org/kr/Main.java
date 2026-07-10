d4a5d11ff783b44b2b76fe566a10ec3467b60a64
Krzysztof Ruta <krzysztof.ruta@play.pl>
2026-07-10 21:04:12 +0200

    enable saving image to file

--- src/main/java/org/kr/Main.java
+++ src/main/java/org/kr/Main.java
@@ -2,20 +2,23 @@ package org.kr;
 
 import com.formdev.flatlaf.FlatLightLaf;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     static void main() {
         IO.println("START init");
         FlatLightLaf.setup();
 
-        JPanel panel1 = new KLPanel();
-        JPanel panel2 = new KLPanel();
-        JPanel panel3 = new KLPanel();
-        JPanel panel4 = new KLPanel();
+        KLPanel panel1 = new KLPanel();
+        KLPanel panel2 = new KLPanel();
+        KLPanel panel3 = new KLPanel();
+        KLPanel panel4 = new KLPanel();
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(900, 700);
@@ -26,6 +29,17 @@ public class Main {
         frame.add(panel3);
         frame.add(panel4);
         frame.setVisible(true);
+
+        try {
+            panel1.saveImage("p1.png");
+            panel1.saveImage("p2.png");
+            panel1.saveImage("p3.png");
+            panel1.saveImage("p4.png");
+            KLPanel panel5 = new KLPanel();
+            panel5.saveImage("p5.png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         IO.println("END init");
     }
@@ -51,14 +65,23 @@ class KLPanel extends JPanel {
             pixelData[BASE_WIDTH * i] = 0x00888888;
             pixelData[BASE_WIDTH * (i+1)-1] = 0x00888888;
         }
+        prepareImage();
+    }
+
+    private void prepareImage() {
+        WritableRaster raster = image.getRaster();
+        raster.setDataElements(0, 0, BASE_WIDTH, BASE_HEIGHT, pixelData);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        WritableRaster raster = image.getRaster();
-        raster.setDataElements(0, 0, BASE_WIDTH, BASE_HEIGHT, pixelData);
+        prepareImage();
         g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
 
+    }
+
+    public void saveImage(String path) throws IOException {
+        ImageIO.write(image, "png", new File(path));
     }
 }
