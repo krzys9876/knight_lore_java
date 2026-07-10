1af8e18e91c9a82c1ceb36ffd78ffc4768e0bcf4
Krzysztof Ruta <krzysztof.ruta@play.pl>
2026-07-10 20:43:16 +0200

    draw dynamically scaled random noise images

--- src/main/java/org/kr/Main.java
+++ src/main/java/org/kr/Main.java
@@ -5,10 +5,11 @@ import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 
 public class Main {
     static void main() {
-        IO.println("START");
+        IO.println("START init");
         FlatLightLaf.setup();
 
         JPanel panel1 = new KLPanel();
@@ -26,34 +27,38 @@ public class Main {
         frame.add(panel4);
         frame.setVisible(true);
 
-
-        //panel.paint(g);
-        //panel.repaint();
-
-        IO.println("END");
+        IO.println("END init");
     }
 }
 
 class KLPanel extends JPanel {
-    final int WIDTH = 400;
-    final int HEIGHT = 300;
-    BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    final int BASE_WIDTH = 256+2;
+    final int BASE_HEIGHT = 192+2;
+    int[] pixelData = new int[BASE_WIDTH * BASE_HEIGHT];
+    BufferedImage image = new BufferedImage(BASE_WIDTH, BASE_HEIGHT, BufferedImage.TYPE_INT_RGB);
 
     public KLPanel() {
         super();
-        setSize(WIDTH, HEIGHT);
+        setSize(BASE_WIDTH, BASE_HEIGHT);
+        for(int i=0; i<pixelData.length; i++){
+            pixelData[i] = (int)(Math.random()*256*256*256);
+        }
+        for(int i=0; i<BASE_WIDTH; i++){
+            pixelData[i] = 0x00888888;
+            pixelData[(BASE_HEIGHT-1)*BASE_WIDTH+i] = 0x00888888;
+        }
+        for(int i=0; i<BASE_HEIGHT; i++){
+            pixelData[BASE_WIDTH * i] = 0x00888888;
+            pixelData[BASE_WIDTH * (i+1)-1] = 0x00888888;
+        }
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics gi = image.getGraphics();
-        gi.setColor(Color.RED);
-        gi.fillRect(0, 0, WIDTH/2, HEIGHT/2);
-        gi.setColor(Color.GREEN);
-        gi.fillRect(WIDTH/2, HEIGHT/2, WIDTH/2, HEIGHT/2);
-
-        g.drawImage(image, 0, 0, null);
+        WritableRaster raster = image.getRaster();
+        raster.setDataElements(0, 0, BASE_WIDTH, BASE_HEIGHT, pixelData);
+        g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
 
     }
 }
