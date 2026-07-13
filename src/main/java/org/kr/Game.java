e1700e2599e7d2443f85d068f50b10bdfffdeb6b
Krzysztof Ruta <krzysztof.ruta@play.pl>
2026-07-13 22:52:18 +0200

    start implementing main menu display

--- src/main/java/org/kr/Game.java
+++ src/main/java/org/kr/Game.java
@@ -20,6 +20,9 @@ public class Game implements Runnable {
     private final DataBlock variables = new DataBlock(0x5BA0, 0x6107 - 0x5BA0 + 1);
     private final DataBlock lookupTable = new DataBlock(0xF100, 0xFFFF - 0xF100 + 1);
     private final DataBlock flags12_1_D16D = InitialData.flags12_1_D16D.copy();
+    private final DataBlock menu_colours_BDA2 = InitialData.menu_colours_BDA2.copy();
+    private final DataBlock menu_xy_BDAA = InitialData.menu_xy_BDAA.copy();
+    private final DataBlock menu_text_BDBA = InitialData.menu_text_BDBA.copy();
 
     // Repaint every fixed interval
     final long repaintIntervalMs = 50;
@@ -120,6 +123,9 @@ public class Game implements Runnable {
         variables.set(0x5BA0, (variables.get(0x5BA0) + variables.get(0x5BA2)) & 0xFF);
         // CALL $D55F    ; {colour is bright yellow on black
         clear_scrn_D55F();
+        // CALL $BD0C    ;
+        do_menu_selection_BD0C();
+
 
         printVariables();
     }
@@ -197,6 +203,40 @@ public class Game implements Runnable {
         for(int hl = 0x4000; hl < 0x4000+0x1800; hl++) { mainMemory.setByteAt(hl, 0); }
     }
 
+    private void do_menu_selection_BD0C() {
+        debugPanel1.append("do_menu_selection_BD0C");
+        // XOR A         ;
+        // LD ($5BB8),A  ;
+        variables.set(0x5BB8, 0);
+        // LD HL,$BDA2   ;
+        // LD B,$08      ; # menu entries
+
+        //; reset flashing attribute
+        //@label=loc_BD15
+        // RES 7,(HL)    ;
+        // INC HL        ; next menu colour entry
+        // DJNZ $BD15    ; {loop until done
+        for (int hl = 0xBDA2; hl < 0xBDA2 + 8; hl++) { menu_colours_BDA2.set(hl, menu_colours_BDA2.get(hl) | 0x7F); }
+        //CALL $D567    ;
+        for (int hl = 0xD8F3; hl < 0xD8F3 + 0x1800; hl++) { shadowMemory.setByteAt(hl, 0); }
+        // CALL $BEB3    ;
+        display_menu_BEB3();
+    }
+
+    private void display_menu_BEB3() {
+        debugPanel1.append("display_menu_BEB3");
+        for(int de1 = 0xBDA2; de1 < 0xBDA2+8; de1++) {
+            int hl2 = 0xBDAA;
+            int de2 = 0xBDBA;
+            variables.set(0x5BB6, menu_colours_BDA2.get(de1));
+            int h = menu_xy_BDAA.get(hl2);
+            int l = menu_xy_BDAA.get(hl2+1);
+            hl2 += 2;
+
+        }
+    }
+
+
     private void printLookupTable() {
         debugTable("Lookup table:", 0xF100, lookupTable.getCopy());
     }
@@ -229,4 +269,15 @@ public class Game implements Runnable {
 
 class InitialData {
     public static final DataBlock flags12_1_D16D = new DataBlock(0xD16D, new int[] {0, 0, 0, 0});
+    public static final DataBlock menu_colours_BDA2 = new DataBlock(0xBDA2, new int[] {0x43,0xC4,0x44,0x44,0x44,0x45,0x47,0x47});
+    public static final DataBlock menu_xy_BDAA = new DataBlock(0xBDAA, new int[] {0x58,0x9F,0x30,0x8F,0x30,0x7F,0x30,0x6F,0x30,0x5F,0x30,0x4F,0x30,0x3F,0x50,0x27});
+    public static final DataBlock menu_text_BDBA = new DataBlock(0xBDBA, new int[] {
+            0x14,0x17,0x12,0x10,0x11,0x1D,0x26,0x15,0x18,0x1B,0x8E,
+            0x01,0x26,0x14,0x0E,0x22,0x0B,0x18,0x0A,0x1B,0x8D,
+            0x02,0x26,0x14,0x0E,0x16,0x19,0x1C,0x1D,0x18,0x17,0x26,0x13,0x18,0x22,0x1C,0x1D,0x12,0x0C,0x94,
+            0x03,0x26,0x0C,0x1E,0x1B,0x1C,0x18,0x1B,0x26,0x26,0x26,0x13,0x18,0x22,0x1C,0x1D,0x12,0x0C,0x94,
+            0x04,0x26,0x12,0x17,0x1D,0x0E,0x1B,0x0F,0x0A,0x0C,0x0E,0x26,0x12,0x92,
+            0x05,0x26,0x0D,0x12,0x1B,0x0E,0x0C,0x1D,0x12,0x18,0x17,0x0A,0x15,0x26,0x0C,0x18,0x17,0x1D,0x1B,0x18,0x95,
+            0x00,0x26,0x1C,0x1D,0x0A,0x1B,0x1D,0x26,0x10,0x0A,0x16,0x8E,
+            0x25,0x26,0x01,0x09,0x08,0x04,0x26,0x0A,0x24,0x0C,0x24,0x10,0xA4});
 }
